ba1c0edccc1aea203f8b63ded547183e5e6ce43b
ZoabKapoor <zoab95@hotmail.com>
2015-11-15 22:04:48 -0800

    modularized print backend

--- src/main/scala/caffeineScript/semantics/PrintBackend.scala
+++ src/main/scala/caffeineScript/semantics/PrintBackend.scala
@@ -9,13 +9,32 @@ package object PrintBackend {
   def execute(program: List[Instruction]) = {program.foreach { x => executeInstruction(x)}}
   
   def executeInstruction(instr: Instruction) = {
-    print(instr.verb.name + "ing " + instr.quantity.amount + " " + instr.quantity.typename + " of " + instr.ingredient.name + "\n")
+      printVerb(instr.verb)
+      printQty(instr.quantity)
+      printIngr(instr.ingredient) 
+      printNL()
+  }
+  
+  def printVerb(verb: Verb): Unit = {
+    print(verb.name + "ing ")
+  }
+  
+  def printQty(quantity: Quantity): Unit = {
+    print(quantity.amount + " " + quantity.typename)
+  }
+  
+  def printIngr(ingredient: Ingredient): Unit = {
+    print(" of " + ingredient.name)
+  }
+  
+  def printNL(): Unit = {
+    print("\n")
   }
   
   // could modularize this into calls processing each part of the AST?
   // traits for printing stuff, 
   
   
-  // Possible backend: map from ingredient to quantities, which can be queried for "how much ingredient do
+  // Possible other backend: map from ingredient to quantities, which can be queried for "how much ingredient do
   // we have"? and updating for a certain ingredient.
 }
